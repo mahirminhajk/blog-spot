5e12fbf094d8fcc60773dda0477fc1d19065ccab
mahirminhajk <kmahirminhaj@gmail.com>
2022-08-22 21:12:34 +0530

    challenge *5

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const PORT = 3000
-
+//'/'
 app.get('/', (req, res) => {
   res.render('home', { hsc: homeStartingContent });
-})
+});
 
+//'about'
+app.get('/about', (req, res) => {
+  res.render('about', { ac: aboutContent });
+});
+
+//'contact'
+app.get('/contact', (req, res) => {
+  res.render('contact', { cc: contactContent });
+});
 
 
 
